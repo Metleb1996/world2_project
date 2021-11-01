ba9ac3198f642f58f71cf4aaa082f9f07025dfb9
Metleb1996 <metlebrustemov1996@gmail.com>
2021-11-01 22:58:58 +0400

    Added new functions for new pages

--- app.py
+++ app.py
@@ -15,6 +15,20 @@ def index():
     }
     return render_template("index.html", cnt=cntx)
 
+@app.route("/ua")
+def user_actions():
+    cntx = {
+        "title":"Login or Register"
+    }
+    return render_template("ua.html", cnt=cntx)
+
+@app.route("/ma")
+def manage_applications():
+    cntx = {
+        "title":"Manage Applications"
+    }
+    return render_template("ma.html", cnt=cntx)
+
 
 if __name__ == "__main__":
     app.run(debug=False)
